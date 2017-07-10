678918c2c677d44a7a2ff77871129039b3f4ec34
leekiea <17413021@qq.com>
2017-07-10 13:05:14 +0800

    add the login user support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'ustudy-app',
@@ -6,4 +6,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     pageTitle: string = '蘑菇云后台管理系统';
+    userName: string = '';
+
+    ngOnInit() : void {
+        console.log("get user name...");
+		this.getUserName();
+    }
+
+    getUserName() {
+		this.fetch((data) => {
+			//cache the list
+			console.log("data: " + JSON.stringify(data));
+			this.userName = data.userName===undefined ? '' : data.userName;
+		});		
+	}
+	
+	fetch(cb) {
+		const req = new XMLHttpRequest();
+		req.open('GET', 'http://47.92.53.57:8080/dashboard/loginId');
+		req.onload = () => {
+			cb(JSON.parse(req.response));
+		};
+		
+		req.send();
+	}	
 }
